74ac6ffe52a5e5c2b5f5dbd643d9df7ce5cdc428
Yujiro <siroidanngo1061@yahoo.co.jp>
2023-02-24 08:17:45 +0000

    0224㈮

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -17,8 +17,7 @@
         
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?
-        family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         
         <!-- Styles -->
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
